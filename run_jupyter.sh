7e3e5a894d525aa81cf51bb0522707022ee10e10
sinazarriess <>
2021-07-19 10:40:15 +0200

    runnning notebook on cluster

--- run_jupyter.sh
+++ run_jupyter.sh
@@ -7,7 +7,6 @@ date;hostname;pwd
 export PATH=/media/compute/homes/szarriess/anaconda3/bin${PATH:+:${PATH}}
 export PATH=/media/compute/vol/cuda/10.1/bin${PATH:+:${PATH}}
 export LD_LIBRARY_PATH=/media/compute/vol/cuda/10.1/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
-                                          _L
 export CUDA_HOME=/media/compute/vol/cuda/10.1
 
 # activate conda env with tensorflow:
